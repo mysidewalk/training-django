46d3d2319be07e74d97524be8d2e26d2f06e6a5b
Peter Freeze <pjfreeze@gmail.com>
2015-02-09 12:26:39 -0600

    Updated slices.py

--- week-1/slices.py
+++ week-1/slices.py
@@ -14,9 +14,9 @@ print sliced_names
 
 # You can create a slicing object with the same three values. The declaration
 # below creates a sliced subset just like the above syntax
+
 to_slice = 'platform intermediate'
-slice_object = slice(0, 8, -1)
-slice_result = to_slice[slice_object]
+slice_result = to_slice[0:8:-1]
 print slice_result
 
 
@@ -26,10 +26,36 @@ print slice_result
 # Write a __getitem__ method that allows you to specify the string version of indexes
 # (e.g. "zero" instead of "0"). (Don't worry about implementing support for negative numbers)
 
-class StringSlices(Object):
-  __init__(self, *args, **kwargs):
-    pass
+class StringSlice(object):
+    def __init__(self, name):
+        self.name = name
 
-  __getitem__(self, value):
-    # Implement your solution here
-    pass
+    def __getitem__(self, value):
+        # Update this method to translate string values into integers that it can use to slice
+        # self.name if value is a slice
+        pass
+
+string_slice = StringSlice('MindMixer')
+result = string_slice['one':'three']
+# Should print the letters 1-2
+print result
+
+
+class Gradebook(object):
+    grades = [
+        {"value": 99, "date": 1},
+        {"value": 87, "date": 2},
+        {"value": 67, "date": 15},
+        {"value": 91, "date": 5},
+        {"value": 81, "date": 9},
+        {"value": 77, "date": 12}
+    ]
+
+    def __getitem__(self, key):
+        # Put your solution here for returning the proper subset of grades based on if they fall
+        # between the start and stop values.
+
+math_class = Gradebook()
+# Should return the grades for any grade with a "date" value between 1 and 9, including 1 and 
+# excluding 9.
+print math_class[1:9]
